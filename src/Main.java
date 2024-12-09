1dedc1ed7f14cd12efbc25a912e01b6464c84835
Prosenjit Mondal <contact.prosen25@gmail.com>
2024-12-09 17:11:47 +0000

    added logic to calculate years and days from minutes

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,23 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
 
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) {
+
+            System.out.println("Invalid Value");
+        } else {
+
+            int days = (int) (minutes / 60) / 24;
+            int years = days / 365;
+            int remainingDays = days % 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
     }
 }
